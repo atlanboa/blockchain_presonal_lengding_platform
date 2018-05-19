7e83ac41d3e3b967d38bffb1c2bce3dc3028d691
김예지 <soph456@naver.com>
2018-05-19 23:07:20 +0900

    update recv function

--- node/server.js
+++ node/server.js
@@ -19,30 +19,36 @@ wss.broadcast = function(data){
         }
     });
 }
-
+function makeCIQ(type){
+    let CIQ={};
+    CIQ.Format='CIQ';
+    if(type=='Array'){
+        CIQ.Array=client.slice();
+    }else{
+        CIQ.Object=client.pop();
+        client.push(CIQ.Object);
+    }
+    return CIQ;
+}
 let recv=function(message){
 
-    /** 메세지가 String이고 그 자체로 사용 가능 할 때 는 그냥 message 변수 자체를 사용
-     * 만약, 메세지가 json으로 들어왔을 때 사용해야하는 함수 -2
-     * result type: Obj, message type: String -2
-    */
-    //var result=JSON.parse(message) -2
-
-    /** 원하는 결과를 가지고 어떤 메세지를 받았을 때, 어떻게 동작해야 하는지 아래에 기술*/
-    
     var msg=JSON.parse(message);
     if(msg.Format=='IAP'){
         //새로운 IP 가 접속했다는 의미
         client.push({
             IP:msg.IP, Port:msg.Port
         });
-        let PID={};
-        PID.Format='PID';
-        PID.Array=client;
-        //this.id=uuid.v4();
-        ws.send(JSON.stringify(PID));
+        var CIQ=makeCIQ('Array');
+        ws.send(JSON.stringify(CIQ));
     }
-
+    else if(msg.Format=='CIS'){
+        if(msg.Status=='Fail'){
+            if(msg.Type=='Array')
+                ws.send(JSON.stringify(makeCIQ('Array')));
+            else
+                ws.send(JSON.stringify(makeCIQ('Object')));
+        }
+    }
 }
 
 function noop(){}
