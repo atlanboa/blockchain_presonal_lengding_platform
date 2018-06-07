a4383078b9bceab2fc71b8d0175e38a2f5bf2a84
김예지 <soph456@naver.com>
2018-06-07 22:25:15 +0900

    라우터 /myroom 추가

--- routes/accounts.js
+++ routes/accounts.js
@@ -114,7 +114,10 @@ router.get('/myroom', function(req,res){
     if(!req.isAuthenticated()){
         res.send('<script>alert("로그인이 필요한 서비스입니다.");function popup(){var url ="/accounts/login";var name = "popup";window.open(url,name,"width=300,height=280,toolbar=no,status=no,location=no,scrollbars=yes,menubar=no,resizable=yes,left=50,right=50");}popup();</script>');
     }else{
-        res.render('accounts/myroom',{user : req.user});
+        var blockchain=require('../node/global.js').blockchain;
+        var object=blockchain.findTransaction(req.user.username);
+        
+        res.render('accounts/myroom',{user : req.user, object:object });
     }
 });
 
